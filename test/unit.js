013d020bef11c90449317b22a90030b097983164
Luke Childs <lukechilds123@gmail.com>
2017-01-16 22:24:20 +0700

    Test DOMContentLoaded event

--- test/unit.js
+++ test/unit.js
@@ -32,3 +32,24 @@ test.cb('Promise resolves with local document', t => {
 	};
 	jsdom.env(config);
 });
+
+test('callback fires if we wait for DOMContentLoaded', async t => {
+	t.plan(1);
+	const config = {
+		html: '',
+		created: (err, window) => domLoaded(() => t.pass(), window.document)
+	};
+	jsdom.env(config);
+});
+
+test.cb('Promise resolves if we wait for DOMContentLoaded', t => {
+	t.plan(1);
+	const config = {
+		html: '',
+		created: (err, window) => domLoaded(window.document).then(() => {
+			t.pass();
+			t.end();
+		})
+	};
+	jsdom.env(config);
+});
