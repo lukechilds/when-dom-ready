8a7b75b6b9e7826633e47134f58155fe561e8385
Luke Childs <lukechilds123@gmail.com>
2017-01-16 17:55:04 +0700

    Test domLoaded Promise resolves

--- test/unit.js
+++ test/unit.js
@@ -5,3 +5,8 @@ test('domLoaded runs the callback', t => {
 	t.plan(1);
 	domLoaded(() => t.pass());
 });
+
+test('domLoaded Promise resolves', async t => {
+	t.plan(1);
+	domLoaded().then(() => t.pass());
+});
