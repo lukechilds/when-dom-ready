73e6ee2b21f7b33da33c266ac0d387ce8647c6b6
Luke Childs <lukechilds123@gmail.com>
2017-01-16 23:57:21 +0700

    Add type test for whenDomReady.resume

--- test/types.js
+++ test/types.js
@@ -8,3 +8,13 @@ test('whenDomReady is a function', t => {
 test('whenDomReady returns a Promise', t => {
 	t.true(whenDomReady() instanceof Promise);
 });
+
+test('whenDomReady.resume is a function', t => {
+	t.is(typeof whenDomReady.resume, 'function');
+});
+
+test('whenDomReady.resume returns a function that returns a promise', t => {
+	const returnValue = whenDomReady.resume();
+	t.is(typeof returnValue, 'function');
+	t.true(returnValue() instanceof Promise);
+});
