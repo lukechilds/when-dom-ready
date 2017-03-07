b4030f75471af2309f3bd4a75fff708f8cbc0eb5
Luke Childs <lukechilds123@gmail.com>
2017-03-07 18:56:43 +0700

    Test Promise value always resolves to undefined

--- test/types.js
+++ test/types.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import jsdom from 'jsdom';
 import whenDomReady from '../';
 
 test('whenDomReady is a function', t => {
@@ -17,4 +18,18 @@ test('whenDomReady.resume returns a function that returns a promise', t => {
 	const returnValue = whenDomReady.resume();
 	t.is(typeof returnValue, 'function');
 	t.true(returnValue() instanceof Promise);
+});
+
+test.cb('Promise value always resolves to undefined', t => {
+	t.plan(2);
+	const config = {
+		html: '',
+		onload: window => {
+			const promises = [];
+			promises.push(whenDomReady(() => 'foo', window.document).then(val => t.is(val, undefined)));
+			promises.push(whenDomReady(window.document).then(val => t.is(val, undefined)));
+			Promise.all(promises).then(() => t.end());
+		}
+	};
+	jsdom.env(config);
 });
