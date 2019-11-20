0f7c2ca2ab79b2522d001e904f24c88afd529b29
Amit Merchant <bullredeyes@gmail.com>
2019-11-20 09:55:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@
 const loadedStates = ['interactive', 'complete'];
 
 // Return Promise
-const whenDomReady = (cb, doc) => new Promise(resolve => {
+const whenDomReady = (callback, doc) => new Promise(resolve => {
 	// Allow doc to be passed in as the lone first param
-	if (cb && typeof cb !== 'function') {
-		doc = cb;
-		cb = null;
+	if (callback && typeof callback !== 'function') {
+		doc = callback;
+		callback = null;
 	}
 
 	// Use global document if we don't have one
 	doc = doc || window.document;
 
 	// Handle DOM load
-	const done = () => resolve(void (cb && setTimeout(cb)));
+	const done = () => resolve(void (callback && setTimeout(callback)));
 
 	// Resolve now if DOM has already loaded
 	// Otherwise wait for DOMContentLoaded
